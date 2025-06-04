2c30c8d65fdd690d5c2c559575dc96bc0c7de8f0
Julio Cesar <juliocesarmartinez1344@gmail.com>
2025-06-03 19:10:28 -0500

    auth flow completed

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -11,11 +11,15 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 import { cn } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { z } from "zod";
+import { Alert, AlertDescription } from "./ui/alert";
 
 const loginSchema = z.object({
   email: z.string().email("Email inválido").min(1, "Email es requerido"),
@@ -26,6 +30,8 @@ export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -34,9 +40,24 @@ export function LoginForm({
     },
   });
 
-  const onSubmit = (data: z.infer<typeof loginSchema>) => {
+  const onSubmit = async (data: z.infer<typeof loginSchema>) => {
     console.log("Login data:", data);
-    // Aquí va la lógica para login
+    const { error } = await authClient.signIn.email({
+      email: data.email,
+      password: data.password,
+      fetchOptions: {
+        onSuccess: () => {
+          console.log("Login successful");
+          setError(null);
+          router.push("/");
+        },
+      },
+    });
+    if (error) {
+      console.error("Login error:", error.message);
+      setError(error.message ?? "");
+      return;
+    }
   };
 
   return (
@@ -51,6 +72,13 @@ export function LoginForm({
                   <p className="text-muted-foreground text-balance">
                     Ingresa con tu cuenta SPA
                   </p>
+                  {error && (
+                    <Alert className="mt-4" variant="destructive">
+                      <AlertDescription>
+                        {error && <span className="text-red-500">{error}</span>}
+                      </AlertDescription>
+                    </Alert>
+                  )}
                 </div>
 
                 <FormField
@@ -98,7 +126,17 @@ export function LoginForm({
                   </span>
                 </div>
 
-                <Button variant="outline" type="button" className="w-full">
+                <Button
+                  variant="outline"
+                  type="button"
+                  className="w-full"
+                  onClick={() =>
+                    authClient.signIn.social({
+                      provider: "google",
+                      callbackURL: "/",
+                    })
+                  }
+                >
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
@@ -110,7 +148,7 @@ export function LoginForm({
 
                 <div className="text-center text-sm">
                   Don&apos;t have an account?{" "}
-                  <a href="#" className="underline underline-offset-4">
+                  <a href="/register" className="underline underline-offset-4">
                     Sign up
                   </a>
                 </div>
--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -10,11 +10,14 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 import { cn } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { Alert, AlertDescription } from "./ui/alert";
 const registerSchema = z.object({
   name: z.string().min(1, "Nombre completo es requerido"),
   email: z.string().email("Email inválido").min(1, "Email es requerido"),
@@ -33,10 +36,26 @@ export function RegisterForm({
       password: "",
     },
   });
-
-  const onSubmit = (data: z.infer<typeof registerSchema>) => {
+  const [error, setError] = React.useState<string | null>(null);
+  const [sucess, setSuccess] = React.useState<string | null>(null);
+  const router = useRouter();
+  const onSubmit = async (data: z.infer<typeof registerSchema>) => {
     console.log("Register data:", data);
-    // Aquí tu lógica de registro
+    const { error } = await authClient.signUp.email({
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      callbackURL: "/",
+    });
+    if (error) {
+      console.error("Registration error:", error.message);
+      setError(error.message ?? "");
+      return;
+    }
+    console.log("Registration successful");
+    setSuccess("Registro exitoso, por favor inicia sesión.");
+    router.push("/login");
+    setError(null);
   };
 
   return (
@@ -51,6 +70,21 @@ export function RegisterForm({
                   <p className="text-muted-foreground text-balance">
                     Únete a SPA
                   </p>
+                  {error && (
+                    <Alert className="mt-4">
+                      <AlertDescription>
+                        {error && <div className="text-red-500">{error}</div>}
+                      </AlertDescription>
+                    </Alert>
+                  )}
+
+                  {sucess && (
+                    <Alert className="mt-4">
+                      <AlertDescription className="text-green-500">
+                        {sucess}
+                      </AlertDescription>
+                    </Alert>
+                  )}
                 </div>
 
                 <FormField
@@ -112,7 +146,17 @@ export function RegisterForm({
                   </span>
                 </div>
 
-                <Button variant="outline" type="button" className="w-full">
+                <Button
+                  variant="outline"
+                  type="button"
+                  className="w-full"
+                  onClick={() =>
+                    authClient.signIn.social({
+                      provider: "google",
+                      callbackURL: "/",
+                    })
+                  }
+                >
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
